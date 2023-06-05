37bab7736e7f88a3dbe230a26b891f0e0f334eda
Alhassan Zainab <azainab390@gmail.com>
2023-06-05 11:55:47 -0700

    Python - Hello, World

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,13 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list.
- * @n: integer.
- * @next: points to the next node.
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Description: singly linked list node structure.
+ * Description: singly linked list node structure
  * 
  */
 typedef struct listint_s
@@ -21,4 +22,4 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
