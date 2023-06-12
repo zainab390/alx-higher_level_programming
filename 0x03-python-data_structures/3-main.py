6c47567b9141c898b78bc3938add8a967e7a30ce
Alhassan Zainab <azainab390@gmail.com>
2023-06-12 06:16:20 -0700

    updated

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -4,3 +4,9 @@ print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_
 my_list = [1, 2, 3, 4, 5]
 print_reversed_list_integer(my_list)
 
+guillaume@ubuntu:~/0x03$ ./3-main.py
+5
+4
+3
+2
+1
