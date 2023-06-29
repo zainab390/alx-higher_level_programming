c02a0ce1e4a1beec55a5d3401caf9099f2a92e95
Alhassan Zainab <azainab390@gmail.com>
2023-06-29 16:34:11 -0700

    Python - Classes and Objects

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""defines a class square"""
+
+class Square:
+    """
+    An square class that accepts a size
+    """
+
+    def __init__(self, size=0):
+        """
+        initialize class with size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        calculate the area of a square
+        """
+        return self.__size ** 2
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,4 @@
+0x06. Python - Classes and Objects
+
+this is an ALX project on python classes and objects.
+
