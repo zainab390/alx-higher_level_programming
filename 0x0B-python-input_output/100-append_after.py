54508a9c768c2c5539f964ee25674bd921f7fcbc
Alhassan Zainab <azainab390@gmail.com>
2023-07-11 10:22:23 -0700

    updated

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 """
-module
+This program take a file and read it, search a string
+and add other string on the next line
 """
 
 
 def append_after(filename="", search_string="", new_string=""):
-        """inserts a line of text to a file, after each line
-           containing a specific string
-        """
-        with open(filename, encoding="utf-8") as my_file:
-            text = my_file.read()
-        text_list = text.split("\n")
-        for i in range(len(text_list)):
-            if search_string in text_list[i]:
-                text_list[i] = text_list[i] + "\n" + new_string
-            elif (i != len(text_list) - 1):
-                text_list[i] = text_list[i] + "\n"
-        text2 = ''.join(text_list)
-        with open(filename, encoding="utf-8", mode="w") as my_file2:
-            my_file2.write(text2)
+    """
+    Open and reads a file, search a string in all the file.
+    and after each ocurrence add other string (new_string)
+    Args:
+    - filename: string
+    - search_string: string
+    - new_string: string
+    """
+
+    with open(filename, mode="r+", encoding="utf-8") as _file:
+        all_text = _file.readlines()
+        new_text = ""
+
+        for line in all_text:
+            new_text += line
+            if (search_string in line):
+                new_text += new_string
+
+        # This method position the cursor to the beginning of the file
+        _file.seek(0)
+        _file.write(new_text)
